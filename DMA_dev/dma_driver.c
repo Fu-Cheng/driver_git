faaf97c5a528210064ae97e1acfe21a93b44666f
Fu-Cheng <photonictsai@gmail.com>
2020-11-10 18:16:50 +0800

    modify

--- DMA_dev/dma_driver.c
+++ DMA_dev/dma_driver.c
@@ -179,9 +179,9 @@ static int dma_open(struct inode *inode,struct file *file){
 	//kernel_device->dma_mask=(u64 *)&dmamask;
 	kernel_device->coherent_dma_mask=DMA_BIT_MASK(32);
     	axidma_addr = dma_alloc_coherent(kernel_device, DMA_LENGTH, &axidma_handle, GFP_KERNEL);
-    	err = request_irq(49, dma_mm2s_irq, IRQF_TRIGGER_RISING, "dma_dev", NULL);
+    	err = request_irq(49, dma_mm2s_irq, IRQF_TRIGGER_MASK, "dma_dev", NULL);
     	printk("err=%d\n",err);
-    	err = request_irq(48, dma_s2mm_irq, IRQF_TRIGGER_RISING, "dma_dev", NULL);
+    	err = request_irq(48, dma_s2mm_irq, IRQF_TRIGGER_MASK, "dma_dev", NULL);
     	printk("err=%d\n",err);
     	return 0;
 }
