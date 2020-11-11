7ba7da59caa6085c99aa59e12dc4f0ad17863bf1
Fu-Cheng <photonictsai@gmail.com>
2020-11-11 10:33:51 +0800

    modify

--- DMA_dev/dma_driver.c
+++ DMA_dev/dma_driver.c
@@ -112,8 +112,8 @@ static int dma_init(void){
 	
 		
     	dma_class=class_create(THIS_MODULE,"dma_dev");
-    	kernel_device=device_create(dma_class, NULL, MKDEV(Major,0), NULL, "dma_dev");
-	
+    	//kernel_device=device_create(dma_class, NULL, MKDEV(Major,0), NULL, "dma_dev");
+	device_create(dma_class, NULL, MKDEV(Major,0), NULL, "dma_dev");
 
 	//major=register_chrdev(0,"dma_dev",&dma_fops);
     	//dma_class= class_create(THIS_MODULE,"dma_dev");
@@ -172,9 +172,10 @@ const int IRQ_NUM2	=49;
 static int dma_open(struct inode *inode,struct file *file){
 	int err;
     	printk("DMA open\n");
-	static const u64 dmamask = DMA_BIT_MASK(32);
-	kernel_device->dma_mask=(u64 *)&dmamask;
-	kernel_device->coherent_dma_mask=DMA_BIT_MASK(32);
+	//static const u64 dmamask = DMA_BIT_MASK(32);
+	//kernel_device->dma_mask=(u64 *)&dmamask;
+	//kernel_device->coherent_dma_mask=DMA_BIT_MASK(32);
+	dma_set_mask(kernel_device, DMA_BIT_MASK(32));
     	axidma_addr = dma_alloc_coherent(kernel_device, DMA_LENGTH, &axidma_handle, GFP_KERNEL);
     	//err = request_irq(IRQ_NUM1, dma_mm2s_irq, IRQF_SHARED, "dma_dev", &IRQ_NUM1);
     	//printk("err=%d\n",err);
