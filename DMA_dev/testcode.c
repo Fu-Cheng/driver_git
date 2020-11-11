d46968d06311fe1e216d7c5d3e7ea1157cddc17f
Fu-Cheng <photonictsai@gmail.com>
2020-11-11 12:52:25 +0800

    modify

--- DMA_dev/testcode.c
+++ DMA_dev/testcode.c
@@ -10,6 +10,7 @@ void delay(void)
     for(i=0;i<20000;i++)
         for(j=0;j<10000;j++);
 }
+unsigned int writearray[1000];
 unsigned int readarray[1000];
 int main(int argc , char ** argv){
 	int fd;
@@ -22,20 +23,18 @@ int main(int argc , char ** argv){
 		printf("open file sucuss\n");
     	delay();
     	for(int i=0;i<1000;i++){
-        	readarray[i]=i+1;
+        	writearray[i]=i+1;
     	}
-    	if(1){
-		write(fd, readarray, 1000*4);
-		if(read(fd, readarray, 1000*4)==0){
-			for(int i=0;i<1000;i++){
-            			printf(" %d",readarray[i]);
-            			readarray[i]=readarray[i]*2;
-			}
-			printf("\n=====================================\n");
-        		printf("======================================\n");
+	write(fd, readarray, 1000*4);
+	if(read(fd, readarray, 1000*4)==0){
+		for(int i=0;i<1000;i++){
+            		printf(" %d",readarray[i]);
+            		readarray[i]=readarray[i]*2;
 		}
+		printf("\n=====================================\n");
+        	printf("======================================\n");
+	}
     	delay();
-    	}
 	close(fd);
     return 0;
 }
