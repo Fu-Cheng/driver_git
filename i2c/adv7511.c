0e897f82af83ab57f07c56b03377f37d3f4a76c6
Fu-Cheng <photonictsai@gmail.com>
2020-11-26 23:04:00 +0800

    modify

--- i2c/adv7511.c
+++ i2c/adv7511.c
@@ -979,7 +979,7 @@ static int adv7511_encoder_init(struct i2c_client *i2c, struct drm_device *dev,
 }
 
 static const struct i2c_device_id adv7511_i2c_ids[] = {
-	{ "adv7511", 0 },
+	{ "fb0", 0 },
 	{ "adv7511w", 0 },
 	{ "adv7513", 0 },
 	{ }
