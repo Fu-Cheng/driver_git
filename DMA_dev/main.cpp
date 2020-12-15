50545bb91f3aafc48f46a5c38327710975c6f79e
Fu-Cheng <photonictsai@gmail.com>
2020-12-15 17:18:39 +0800

    modify

--- DMA_dev/main.cpp
+++ DMA_dev/main.cpp
@@ -19,8 +19,8 @@ void delay(void)
     for(i=0;i<20000;i++)
         for(j=0;j<10000;j++);
 }
-unsigned int write_array[1000];
-unsigned int read_array[1000];
+unsigned int write_array[69];
+unsigned int read_array[5];
 
 int main(int argc , char ** argv){
 	
@@ -32,32 +32,30 @@ int main(int argc , char ** argv){
 	}
     	else
 		printf("open file sucuss\n");
-    	delay();
 	
 
-	string aaa;
-	aaa="wegt";
-	int b=0;
-	for (int i=0; i<4; i++)
-		b=(b<<8)+(int)aaa[i];
-
-	write_array[0]=b;
-    	for(int i=1;i<1000;i++){
-        	write_array[i]=i+1;
+	write_array[0]=str2ascii("wlsz");
+	write_array[1]=32;
+	write_array[2]=str2ascii("blsz");
+	write_array[3]=64;
+	write_array[4]=str2ascii("wegt");
+	
+    	for(int i=5;i<69;i++){
+        	write_array[i]=i;
     	}
 
-	write(fd, write_array, 4*1000);
-	if(read(fd, read_array, 4*1000)==0){
+	write(fd, write_array, 4*69);
+	if(read(fd, read_array, 4*5)==0){
 		//printf("read: %d", read_element);
 		
-		for(int i=0;i<1000;i++)
+		for(int i=0;i<5;i++)
             		printf(" %d",read_array[i]);
 
 		
 		printf("\n=====================================\n");
         	printf("======================================\n");
 	}
-    	delay();
+
 	close(fd);
 
 	/*
@@ -74,4 +72,12 @@ int main(int argc , char ** argv){
 	//cout<<typeid(string).name()<<endl;
 	*/
     	return 0;
+}
+
+
+int str2ascii(string input){
+	int output=0;
+	for (int i=0; i<4; i++)
+		output=(b<<8)+(int)input[i];
+	return output;
 }
