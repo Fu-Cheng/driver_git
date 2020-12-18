6279fd3de17bafbeae52887df79eed0cc7f1f7b5
Fu-Cheng <photonictsai@gmail.com>
2020-12-18 21:53:58 +0800

    modify

--- DMA_dev/main.cpp
+++ DMA_dev/main.cpp
@@ -19,52 +19,24 @@ void delay(void)
     for(i=0;i<20000;i++)
         for(j=0;j<10000;j++);
 }
-unsigned int write_array[136];
-unsigned int read_array[64];
 
+void to_cim(unsigned int&)
 int str2ascii(string input);
 
 int main(int argc , char ** argv){
 	
-	int fd;
-    	fd = open("/dev/dma_dev",O_RDWR);
-    	if(fd<0){ 
-		printf("can not open file\n");
-		while(1);
-	}
-    	else
-		printf("open file sucuss\n");
 	
-
+	unsigned int write_array[69];
 	write_array[0]=str2ascii("wlsz");
 	write_array[1]=32;
 	write_array[2]=str2ascii("blsz");
 	write_array[3]=32;
-	write_array[4]=str2ascii("sfsz");
-	write_array[5]=32;
 
-	write_array[6]=str2ascii("wegt");
-    	for(int i=7;i<71;i++)
-		write_array[i]=i-6;
+	write_array[4]=str2ascii("wegt");
+    	for(int i=5;i<69;i++)
+		write_array[i]=i;
 	
-	write_array[71]=str2ascii("inpt");
-	for(int i=72;i<136;i++)
-		in_cim[i]=i-71;
-
-	delay;
-
-	
-	if(read(fd, read_array, 4*64)==0){
-		//printf("read: %d", read_element);
-		
-		for(int i=0;i<64;i++)
-            		printf("%x\n",read_array[i]);
-
-		printf("\n=====================================\n");
-
-	}
-
-	close(fd);
+	to_cim(write_array);
 
 	/*
 	string aaa;
@@ -80,6 +52,27 @@ int main(int argc , char ** argv){
 	//cout<<typeid(string).name()<<endl;
 	*/
     	return 0;
+}
+
+void to_cim(unsigned int& write_array){
+	unsigned int read_array[64];
+	int fd;
+    	fd = open("/dev/dma_dev",O_RDWR);
+    	if(fd<0){ 
+		printf("can not open file\n");
+		while(1);
+	}
+    	else
+		printf("open file sucuss\n");
+	delay;
+	
+	if(read(fd, read_array, 4*64)==0){
+		for(int i=0;i<64;i++)
+            		printf("%x\n",read_array[i]);
+		printf("=====================================\n");
+
+	}
+	close(fd);	
 }
 
 
